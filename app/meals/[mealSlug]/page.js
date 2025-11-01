8d875c46c870c613f709777c63134c6e4cdfcc4d
Cortalo <Cortalo@outlook.com>
2025-11-01 16:16:35 +0100

    file based routers

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+// localhost:3000/meals/share -> go to share
+// localhost:3000/meals/others -> go to mealSlug
+export default function MealDetailsPage() {
+  return (
+    <h1>Meal Details</h1>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      {/* use Link so that it doesn't refresh the whole app */}
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Share Meal</Link></p>
+      <p><Link href="/community">Community</Link></p>
     </main>
   );
 }
